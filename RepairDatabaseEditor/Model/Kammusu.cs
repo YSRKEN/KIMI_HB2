466f3c48546d89c99f29506da91813b2a4f91af9
YSRKEN <ysr.ken@gmail.com>
2018-07-30 23:14:36 +0900

    艦娘データを保持するModelを作成

---
+++ RepairDatabaseEditor/Model/Kammusu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairDatabaseEditor.Model
+{
+    /// <summary>
+    /// 艦娘の情報
+    /// </summary>
+    public class Kammusu
+    {
+        /// <summary>
+        /// 艦船ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 艦名
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
--- RepairDatabaseEditor/ViewModel/MainViewModel.cs
+++ RepairDatabaseEditor/ViewModel/MainViewModel.cs
@@ -1,14 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using RepairDatabaseEditor.Model;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace RepairDatabaseEditor.ViewModel
 {
+    /// <summary>
+    /// メイン画面のViewModel
+    /// </summary>
     class MainViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 艦娘一覧
+        /// </summary>
+        public ObservableCollection<Kammusu> KammusuList { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainViewModel()
+        {
+            KammusuList = new ObservableCollection<Kammusu>();
+            KammusuList.Add(new Kammusu() { Id = 0, Name = "○" });
+            KammusuList.Add(new Kammusu() { Id = 1, Name = "吹雪" });
+        }
     }
 }
