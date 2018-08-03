eef23a36a7075f866676a499a170b7acc86c424b
YSRKEN <ysr.ken@gmail.com>
2018-08-04 00:54:27 +0900

    セレクトボックスを選択すると、下の内容が書き換わるようにした

--- RepairDatabaseEditor/ViewModel/MainViewModel.cs
+++ RepairDatabaseEditor/ViewModel/MainViewModel.cs
@@ -1,8 +1,9 @@
-﻿using RepairDatabaseEditor.Model;
+﻿using Reactive.Bindings;
+using RepairDatabaseEditor.Model;
 using RepairDatabaseEditor.Service;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-
+using System;
 
 namespace RepairDatabaseEditor.ViewModel
 {
@@ -21,21 +22,50 @@ namespace RepairDatabaseEditor.ViewModel
         /// <summary>
         /// 艦娘一覧
         /// </summary>
-        public ObservableCollection<Kammusu> KammusuList { get; set; }
+        public ObservableCollection<Kammusu> KammusuList { get; }
 
         /// <summary>
         /// 装備一覧
         /// </summary>
-        public ObservableCollection<Weapon> WeaponList { get; set; }
+        public ObservableCollection<Weapon> WeaponList { get; }
+
+        /// <summary>
+        /// 選択中の艦娘の名前
+        /// </summary>
+        public ReactiveProperty<Kammusu> SelectedKammusu { get; set; } = new ReactiveProperty<Kammusu>(new Kammusu());
+
+        /// <summary>
+        /// 選択中の装備の名前
+        /// </summary>
+        public ReactiveProperty<Weapon> SelectedWeapon { get; set; } = new ReactiveProperty<Weapon>(new Weapon());
+
+        /// <summary>
+        /// 艦娘の名前
+        /// </summary>
+        public ReactiveProperty<string> KammusuName { get; } = new ReactiveProperty<string>("");
+
+        /// <summary>
+        /// 装備の名前
+        /// </summary>
+        public ReactiveProperty<string> WeaponName { get; } = new ReactiveProperty<string>("");
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public MainViewModel(DataStore dataStore)
         {
+            // リストボックスに標示するデータを読み込み
             this.dataStore = dataStore;
             KammusuList = new ObservableCollection<Kammusu>(dataStore.Kammusus);
             WeaponList = new ObservableCollection<Weapon>(dataStore.Weapons);
+
+            // 選択変更時の処理を記述
+            SelectedKammusu.Subscribe(value => {
+                KammusuName.Value = value.Name;
+            });
+            SelectedWeapon.Subscribe(value => {
+                WeaponName.Value = value.Name;
+            });
         }
     }
 }
